c684cbc1afe4b0d04e07dee1c5bebcbb50fc991d
kitani-y <nativetofu@gmail.com>
2024-06-14 00:12:16 +0900

    javascriptを追加しました

---
+++ passing_mark.js
@@ -0,0 +1,15 @@
+function calculate(){
+    let num1 = document.getElementById('textbox1').value;
+    let num2 = document.getElementById('textbox2').value;
+
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    let nitaki = 100 - num1;
+
+    let scr = ((120 - num2) - nitaki) / 0.7;
+
+    scr = Math.ceil(scr);
+
+    document.getElementById('result').innerText = '次は' + scr + '点取りましょう';
+}
